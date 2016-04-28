6e14b598b3f747cd50552efa2a088bf9373e68f6
jasonli822 <lihu822@hotmail.com>
2016-04-28 10:03:02 +0800

    Initial commit

---
+++ app/src/main/java/chapter1/ImageLoader.java
@@ -0,0 +1,74 @@
+package chapter1;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.util.LruCache;
+import android.widget.ImageView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by jasonli822 on 2016/4/25.
+ * 图片加载类
+ * 原始需求：主管的要求很简单，要小民实现图片加载，并且要讲图片缓存起来。
+ */
+public class ImageLoader {
+    // 图片缓存
+    LruCache<String, Bitmap> mImageCache;
+    // 线程池，线程数量为CPU的数量
+    ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    // 提供一个构造函数
+    public ImageLoader() {
+        initImageCache();
+    }
+
+    private void initImageCache() {
+        // 计算可使用的最大内存
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        // 取四份之一的可用内存作为缓存
+        final int cacheSize = maxMemory / 4;
+        mImageCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+            }
+        };
+    }
+
+    // 显示图片
+    public void displayImage(final String url, final ImageView imageView) {
+        imageView.setTag(url);
+        mExecutorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = downloadImage(url);
+                if (bitmap == null) {
+                   return;
+                }
+                if (imageView.getTag().equals(url)) {
+                    imageView.setImageBitmap(bitmap);
+                }
+                mImageCache.put(url, bitmap);
+            }
+        });
+    }
+
+    // 图片下载
+    public Bitmap downloadImage(String imageUrl) {
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(imageUrl);
+            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            bitmap = BitmapFactory.decodeStream(conn.getInputStream());
+            conn.disconnect();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+}
---
+++ app/src/main/java/io/github/jasonli822/android_sourcecode_design_pattern/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.jasonli822.android_sourcecode_design_pattern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
