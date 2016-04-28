ac231a4e12d0d6805572b2bdbad12a3ff6c2713e
jasonli822 <lihu822@hotmail.com>
2016-04-28 10:33:17 +0800

    单一职责原则，将ImageLoader一分为二，ImageLoader只负责图片加载的逻辑，而ImageCache只负责处理图片缓存的逻辑，这样ImageLoader的代码量变少了，职责也清晰了。

--- app/src/main/java/chapter1/ImageLoader.java
+++ app/src/main/java/chapter1/ImageLoader.java
@@ -18,30 +18,17 @@ import java.util.concurrent.Executors;
  */
 public class ImageLoader {
     // 图片缓存
-    LruCache<String, Bitmap> mImageCache;
+    ImageCache mImageCache = new ImageCache();
     // 线程池，线程数量为CPU的数量
     ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-    // 提供一个构造函数
-    public ImageLoader() {
-        initImageCache();
-    }
-
-    private void initImageCache() {
-        // 计算可使用的最大内存
-        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-        // 取四份之一的可用内存作为缓存
-        final int cacheSize = maxMemory / 4;
-        mImageCache = new LruCache<String, Bitmap>(cacheSize) {
-            @Override
-            protected int sizeOf(String key, Bitmap bitmap) {
-                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
-            }
-        };
-    }
-
-    // 显示图片
+    // 加载图片
     public void displayImage(final String url, final ImageView imageView) {
+        Bitmap bitmap = mImageCache.get(url);
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
         imageView.setTag(url);
         mExecutorService.submit(new Runnable() {
             @Override
