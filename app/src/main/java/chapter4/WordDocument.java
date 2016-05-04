dff3c9db8778d7bae570967001986ece94d4e44f
jasonli822 <lihu822@hotmail.com>
2016-05-04 16:43:08 +0800

    使程序运行更高效--原型模式

---
+++ app/src/main/java/chapter4/WordDocument.java
@@ -0,0 +1,57 @@
+package chapter4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jasonli822 on 2016/5/4.
+ * 文档类型，扮演的是ConcretePrototype角色，而cloneable是代表prototype角色
+ */
+public class WordDocument implements Cloneable {
+    // 文本
+    private String mText;
+    // 图片名列表
+    private ArrayList<String> mImages = new ArrayList<String>();
+
+    public WordDocument() {
+        System.out.println("------ WordDocument构造函数 ------");
+    }
+
+    @Override
+    protected WordDocument clone() {
+        try {
+            WordDocument doc = (WordDocument) super.clone();
+            doc.mText = this.mText;
+            doc.mImages = this.mImages;
+            return doc;
+        } catch(Exception e) {
+        }
+        return null;
+    }
+
+    public String getText() {
+        return mText;
+    }
+    public void setText(String mText) {
+        this.mText = mText;
+    }
+    public List<String> getImages() {
+        return mImages;
+    }
+    public void addImage(String image) {
+        this.mImages.add(image);
+    }
+
+    /**
+     * 打印文档内容
+     */
+    public void showDocument() {
+        System.out.println("------ Word Content Start ------");
+        System.out.println("Text :" + mText);
+        System.out.println("Image List: ");
+        for (String imgName : mImages) {
+            System.out.println("image name :" + imgName);
+        }
+        System.out.println("------ Word Content End ------");
+    }
+}
